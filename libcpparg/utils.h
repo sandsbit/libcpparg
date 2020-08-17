aec4121eb2edb853ea8e6a47ce054d3b8942e81e
Nikita Serba <nikitaserba@icloud.com>
2020-08-17 22:38:40 +0300

    Remove visibility

--- libcpparg/utils.h
+++ libcpparg/utils.h
@@ -53,15 +53,8 @@
             #define DLL_PUBLIC __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
         #endif
     #endif
-    #define DLL_LOCAL
 #else
-    #if __GNUC__ >= 4
-        #define DLL_PUBLIC __attribute__ ((visibility ("default")))
-        #define DLL_LOCAL  __attribute__ ((visibility ("hidden")))
-    #else
-        #define DLL_PUBLIC
-        #define DLL_LOCAL
-    #endif
+    #define DLL_PUBLIC
 #endif
 
 #endif //LIBCPPARG_UTILS_H
