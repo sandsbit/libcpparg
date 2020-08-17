7294a50f1dcf6309ff798d71a656dcbd1a236e2d
Nikita Serba <nikitaserba@icloud.com>
2020-08-17 22:30:19 +0300

    Add library macros

---
+++ libcpparg/utils.h
@@ -0,0 +1,67 @@
+/**
+ *  _     _ _
+ * | |   (_) |__   ___ _ __  _ __   __ _ _ __ __ _
+ * | |   | | '_ \ / __| '_ \| '_ \ / _` | '__/ _` |
+ * | |___| | |_) | (__| |_) | |_) | (_| | | | (_| |
+ * |_____|_|_.__/ \___| .__/| .__/ \__,_|_|  \__, |
+                    |_|   |_|              |___/
+ *
+ * Yet another C++ powerful command line argument parsing library.
+ * Copyright (C) 2020 Nikita Serba. All rights reserved
+ * https://github.com/sandsbit/libcpparg/
+ *
+ *  This file is part of libcpparg.
+ *
+ *  libcpparg is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  libcpparg is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  and GNU Lesser General Public License along with libcpparg.  If not,
+ *  see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIBCPPARG_UTILS_H
+#define LIBCPPARG_UTILS_H
+
+#if __cplusplus < 201703L
+    #error Invalid C++ standard version: C++17 is required.
+#endif
+
+#if __cplusplus == 201703L
+    #warning Using C++20 is recomended. C++17 support will be removed in future versions.
+#endif
+
+
+#if defined _WIN32 || defined __CYGWIN__
+    #ifdef BUILDING_DLL
+        #ifdef __GNUC__
+            #define DLL_PUBLIC __attribute__ ((dllexport))
+        #else
+            #define DLL_PUBLIC __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
+        #endif
+    #else
+        #ifdef __GNUC__
+            #define DLL_PUBLIC __attribute__ ((dllimport))
+        #else
+            #define DLL_PUBLIC __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
+        #endif
+    #endif
+    #define DLL_LOCAL
+#else
+    #if __GNUC__ >= 4
+        #define DLL_PUBLIC __attribute__ ((visibility ("default")))
+        #define DLL_LOCAL  __attribute__ ((visibility ("hidden")))
+    #else
+        #define DLL_PUBLIC
+        #define DLL_LOCAL
+    #endif
+#endif
+
+#endif //LIBCPPARG_UTILS_H
